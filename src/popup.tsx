5ed4fa0ec82feecabb42471067efc09d14165c8a
Guocork <g1024536444@gmail.com>
2024-04-25 21:20:32 +0800

    实现 文字显示效果

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,24 +1,51 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
+import Typed from 'typed.js';
+
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  // const [data, setData] = useState("")
+  const el = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['a new web3 wallet', 'Daism', 'ignite web3.0'],
+      typeSpeed: 100,
+      loop: true,
+      backSpeed: 50,
+      smartBackspace: true,
+      showCursor: false,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension! hhhhhhh 222
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    // <div
+    //   style={{
+    //     padding: 50
+    //   }}>
+    //   <h2>
+    //     Welcome to {""}
+    //     <br />
+    //     <a href="https://test.daism.io/" target="_blank">
+    //     Basique
+    //     </a>{""}
+    //     <br /> 
+    //     A new web3 wallet
+    //   </h2>
+    //   <input onChange={(e) => setData(e.target.value)} value={data} />
+    // </div>
+    <div style={{ width: '300px', height: '500px',display: 'flex',justifyContent: 'center', alignItems: 'center'}}>
+      <div style={{textAlign:'center'}}>
+        <h1> Welcome to </h1>
+        <h1> Basique </h1>
+        <div className='conmtainer1'>
+          <h1 ref={el}></h1>
+        </div>
+      </div>
     </div>
   )
 }
